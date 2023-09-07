55d7e2b9620157077c5b4fb4a881efc4d1a6814f
shashi kumar verma <shashik.verma@cloudactivelabs.in>
2023-09-07 10:46:46 +0530

    bg changed

--- src/App.js
+++ src/App.js
@@ -11,14 +11,15 @@ import { useState } from 'react';
 function App() {
   
   return (
-   <div >
+   <div style={{backgroundColor:'red' , height:'100vh'}}>
      <BrowserRouter >
    <NavBar/>
    <Routes>
+   <Route exact path='/CreateTodo' element={<CreateTodo/>}/>
       <Route exact path='/' element={<AllTodo/>}/>
       <Route exact path='/Login' element={<Login/>}/>
       <Route exact path='/Signup' element={<Signup/>}/>
-      <Route exact path='/CreateTodo' element={<CreateTodo/>}/>
+   
       <Route exact path='/*' element={<PageNotFound/>}/>
       
    </Routes>
