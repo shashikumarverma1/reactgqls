13177c5b0736d717f8343c0caf028536ed488c7c
shashi kumar verma <shashik.verma@cloudactivelabs.in>
2023-09-07 11:13:00 +0530

    bg green

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { useState } from 'react';
 function App() {
   
   return (
-   <div style={{backgroundColor:'red' , height:'100vh'}}>
+   <div style={{backgroundColor:'green' , height:'100vh'}}>
      <BrowserRouter >
    <NavBar/>
    <Routes>
